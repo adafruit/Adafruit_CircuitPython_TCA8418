b9114247aa9da11980f477aa72a3e5026526246a
lady ada <limor@ladyada.net>
2022-08-17 00:17:14 -0400

    clean up

--- examples/tca8418_gpiobutton.py
+++ examples/tca8418_gpiobutton.py
@@ -4,25 +4,23 @@
 
 import time
 import board
-import adafruit_tca8418
+from adafruit_tca8418 import TCA8418
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
+tca = TCA8418(i2c)
 
-from adafruit_debug_i2c import DebugI2C
-debug_i2c = DebugI2C(i2c)
-
-tca = adafruit_tca8418.TCA8418(debug_i2c)
-
+# setup 2 gpio: LED on R0 and button in R1
 OUTPIN = TCA8418.R0
 INPIN = TCA8418.R1
-
 tca.gpio_mode[OUTPIN] = True
 tca.gpio_mode[INPIN] = True
 
+# one is output, other is input with pullup
 tca.gpio_direction[OUTPIN] = True
 tca.gpio_direction[INPIN] = False
 tca.pullup[INPIN] = True
 
+# have LED mirror button
 while True:
     tca.output_value[OUTPIN] = tca.input_value[INPIN]
     time.sleep(0.01)
