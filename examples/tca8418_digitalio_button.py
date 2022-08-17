37fb75dd0bd74e261fa682e67ecc0367de66bb5f
lady ada <limor@ladyada.net>
2022-08-17 00:15:26 -0400

    blink and digitalio button demo

---
+++ examples/tca8418_digitalio_button.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022 ladyada for Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
+
+import time
+import board
+import digitalio
+from adafruit_tca8418 import TCA8418
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+tca = TCA8418(i2c)
+
+# get a 'digitalio' like pins from the tca
+led = tca.get_pin(TCA8418.R0)
+button = tca.get_pin(TCA8418.R1)
+
+# Setup R0 as an output that's at a low logic level default
+led.switch_to_output(value = False)
+# Setup R1 as an input with pullup
+button.switch_to_input(pull=digitalio.Pull.UP)
+
+while True:
+    led.value = button.value
+    time.sleep(0.01)  # debounce
--- examples/tca8418_gpioblink.py
+++ examples/tca8418_gpioblink.py
@@ -7,17 +7,14 @@ import board
 from adafruit_tca8418 import TCA8418
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
+tca = TCA8418(i2c)
 
-from adafruit_debug_i2c import DebugI2C
-debug_i2c = DebugI2C(i2c)
-
-tca = TCA8418(debug_i2c)
-
+# setup R0 as an output GPIO
 OUTPIN = TCA8418.R0
-
 tca.gpio_mode[OUTPIN] = True
 tca.gpio_direction[OUTPIN] = True
 
+# blink it!
 while True:
     tca.output_value[OUTPIN] = True
     time.sleep(0.1)
